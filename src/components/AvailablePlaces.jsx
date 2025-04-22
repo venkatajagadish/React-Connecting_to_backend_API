eac522b4d9e9aa3531357de380f0c288d4188563
venkatajagadish <jagadishreddy445@gmail.com>
2025-04-22 18:01:23 +0530

    catching errors while making api calls and displaying appropriate message to user

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,21 +1,38 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import ErrorPage from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [fetchingData, setFetchingData] = useState(false);
-
+  const [errorMessage, setError] = useState("");
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     async function fetChPlaces() {
       setFetchingData(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+        const resData = await response.json();
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError(error);
+      }
       setFetchingData(false);
     }
     fetChPlaces();
   }, []);
+  if (errorMessage) {
+    return (
+      <ErrorPage
+        title="Some error occurred"
+        message={errorMessage.message || "Fetching places gave error"}
+      />
+    );
+  }
+
   return (
     <Places
       title="Available Places"
