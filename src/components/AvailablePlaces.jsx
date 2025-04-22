05bf352741557c8e92f09cef526789082daf77e6
venkatajagadish <jagadishreddy445@gmail.com>
2025-04-22 15:17:18 +0530

    using async await for api calls and added fetching data text as fall back during the api operation

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,22 +2,27 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [fetchingData, setFetchingData] = useState(false);
+
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetChPlaces() {
+      setFetchingData(true);
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setFetchingData(false);
+    }
+    fetChPlaces();
   }, []);
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
       fallbackText="No places available."
+      isLoading={fetchingData}
+      loadingText="Fetching the places data"
       onSelectPlace={onSelectPlace}
     />
   );
